6564a12d180a5e5065d64e5a453b06fba47e882d
Greg12348 <mphy3600387123@gmail.com>
2024-08-23 13:33:12 -0400

    First commit~

---
+++ app/__init__.py
@@ -0,0 +1,53 @@
+from flask import Flask
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+import logging
+from logging.handlers import SMTPHandler, RotatingFileHandler
+import os
+from flask_mail import Mail
+from flask_bootstrap import Bootstrap
+from elasticsearch import Elasticsearch
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+app.elasticsearch = Elasticsearch([app.config['ELASTICSEARCH_URL']]) \
+    if app.config['ELASTICSEARCH_URL'] else None
+
+db = SQLAlchemy(app)  # 代表database instance
+migrate = Migrate(app, db)  # migrate engine instance
+login = LoginManager(app)
+login.login_view = 'login'  # 高速Flask-login负责登录的view function
+mail = Mail(app)
+bootstrap = Bootstrap(app)
+
+from app import routes, models, errors
+
+if not app.debug:
+    if app.config['MAIL_SERVER']:
+        auth = None
+        if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+            auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+        secure = None
+        if app.config['MAIL_USE_TLS']:
+            secure = ()
+        mail_handler = SMTPHandler(
+            mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+            fromaddr='no-reply@' + app.config['MAIL_SERVER'],
+            toaddrs=app.config['ADMINS'], subject='Dynamicblog Failure',
+            credentials=auth, secure=secure)
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
+
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/dynamicblog.log', maxBytes=10240, backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Dynamicblog startup')
